4d651e75275c50ca74465469f251e03de4852b63
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-05-20 02:05:59 +0900

    [modify] The PMI is calculated by separating CV.

--- R/psa/pmi.R
+++ R/psa/pmi.R
@@ -15,14 +15,24 @@ PMI <- function(x, y, N1, N2, V1, V2, pair.freq, seg.freq) {
   #
   # Returns:
   #   the PMI velue of the segment pair (x, y)
- 
+  
   f.xy <- pair.freq[x, y]
   f.x  <- seg.freq[x]
   f.y  <- seg.freq[y]
   
-  p.xy <- (f.xy + 1) / (N1 + V1)  # probability of the co-occurrence frequency of xy
-  p.x  <- (f.x + 1) / (N2 + V2)  # probability of the occurrence frequency of x
-  p.y  <- (f.y + 1) / (N2 + V2)  # probability of the occurrence frequency of y
+  if (sum(C == y)) {
+    # consonant
+    cv <- 1
+  } 
+  else if (sum(V == y)) {
+    # vowel
+    cv <- 2
+  }
+  
+  p.xy <- (f.xy + 1) / (N1[cv] + V1[cv])  # probability of the co-occurrence frequency of xy
+  p.x  <- (f.x + 1) / (N2[cv] + V2[cv])  # probability of the occurrence frequency of x
+  p.y  <- (f.y + 1) / (N2[cv] + V2[cv])  # probability of the occurrence frequency of y
+  
   pmi  <- log2(p.xy / (p.x * p.y))  # calculating the pmi
   
   return(pmi)
@@ -57,17 +67,49 @@ UpdatePMI <- function(psa.list, s) {
   seg.pair.freq.mat <- MakeFreqMat(seg.vec, seg.pair.mat, corpus)
   seg.freq.vec      <- MakeFreqVec(seg.vec, corpus)
   
-  N1 <- dim(corpus)[2]  # number of the aligned segments
-  N2 <- N1 * 2  # number of segments in the aligned segments
-  V1 <- length(unique(paste(corpus[1, ], corpus[2, ])))  # number of segment pair types
-  V2 <- length(unique(as.vector(corpus))) # number of symbol types
-
+  N1.C <- 0
+  corpus.C <- NULL
+  
+  N1.V <- 0  
+  corpus.V <- NULL
+  
+  C.num <- length(C)  
+  for (i in 1:C.num) {
+    N1.C <- N1.C + sum(C[i] == corpus[2, ])
+    corpus.C <- cbind(corpus.C, corpus[, which(C[i] == corpus[2, ])])
+  }
+  
+  V.num <- length(V)  
+  for (i in 1:V.num) {
+    N1.V <- N1.V + sum(V[i] == corpus[2, ])
+    corpus.V <- cbind(corpus.V, corpus[, which(V[i] == corpus[2, ])])
+  }
+  
+  N2.C <- N1.C * 2  # number of segments in the aligned segments
+  N2.V <- N1.V * 2  # number of segments in the aligned 
+  
+  V1.C <- length(unique(paste(corpus.C[1, ], corpus.C[2, ])))  # number of segment pair types
+  V1.V <- length(unique(paste(corpus.V[1, ], corpus.V[2, ])))  # number of segment pair types
+  
+  V2.C <- length(unique(as.vector(corpus.C))) # number of symbol types
+  V2.V <- length(unique(as.vector(corpus.V))) # number of symbol types
+  
+  # for check the abave process
+  if (0) {
+    N1 <- dim(corpus)[2]  # number of the aligned segments
+    N2 <- N1 * 2  # number of segments in the aligned segments
+    V1 <- length(unique(paste(corpus[1, ], corpus[2, ])))  # number of segment pair types
+    V2 <- length(unique(as.vector(corpus)))  # number of symbol types
+  }
+  
   # Calculate the PMI for all segment pairs.
   pmi.list <- foreach(i = 1:seg.pair.num) %dopar% {
     
     x <- seg.pair.mat[i, 1]
     y <- seg.pair.mat[i, 2]
-    pmi <- PMI(x = x, y = y, N1 = N1, N2 = N2, V1 = V1, V2 = V2,
+    pmi <- PMI(x = x, y = y, 
+               N1 = c(N1.C, N1.V), N2 = c(N2.C, N2.V),
+               V1 = c(V1.C, V1.V), V2 = c(V2.C, V2.V),
                pair.freq = seg.pair.freq.mat, seg.freq = seg.freq.vec)
     
     score.vec     <- list()
@@ -76,7 +118,7 @@ UpdatePMI <- function(psa.list, s) {
     score.vec$pmi <- pmi
     return(score.vec)
   }
-
+  
   # Invert the PMI for all segment pairs.
   score.tmp <- foreach(i = 1:seg.pair.num, .combine = c) %dopar% {
     -pmi.list[[i]]$pmi
