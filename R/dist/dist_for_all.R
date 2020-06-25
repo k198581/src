650b03a77999cc54b48baa3de248a4eec044e7e8
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-06-25 20:12:22 +0900

    j.vec

--- R/dist/dist_for_all.R
+++ R/dist/dist_for_all.R
@@ -6,13 +6,14 @@ source("dist/tools/dist.R")
 
 load("all_list.RData")
 load("i.vec.RData")
+load("j.vec.RData")
 
 dist_for_all <- function(method="lv") {
   
   r.pairs <- t(combn(95, 2))
   N <- dim(r.pairs)[1]
   
-  psa.list <- foreach (i = i.vec) %dopar% {
+  psa.list <- foreach (i = j.vec) %dopar% {
     
     k <- r.pairs[i, 1]
     l <- r.pairs[i, 2]
